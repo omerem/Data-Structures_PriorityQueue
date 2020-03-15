2746bb4f817926d75ef46400f4120b9b10adbdfb
omerem <56478966+omerem@users.noreply.github.com>
2020-03-15 19:41:26 +0200

    Update Maman16.java

--- code/Maman16.java
+++ code/Maman16.java
@@ -67,29 +67,26 @@ class Maman16
         return Math.floor((Math.random()*1024));
     }
     /**
-     * a method that take care of the edge case of k=0.
-     * In a vacuous truth (BE'OFEN REK) the method does print the
-     * k=0 lowest number in the checkpoints. Likewise, the method
-     * does rtuen a list of the smallest k=0 numbers.
+     * A method that take care of the edge case of k=0.
      */
     private static double[] kIsZero ()
     {
             return new double[0];
     }
     /**
-     * the method scans the numbers from the given arrey into pq, until
+     * The method scans the numbers from the given arrey into pq, until
      * there's a checkpoint. The method returns the number of numbers that
-     * have been scanned from the given arrey fter the itteration of
+     * have been scanned from the given arrey after the itteration of
      * this method.
-     * @param scanned the number of numbers that have been scanned
-     * from the given arrey so far.
-     * @param checkpoint the next checkpoint - there, the algorithm need
-     * to run pq.printkMin().
-     * @param arr the given array
-     * @param pq the priority queue that all the numbers that have been scanned
+     * @param scanned The number of numbers that have been scanned
+     * from the given array so far.
+     * @param checkpoint When reaching this checkpoint, the algorithm
+     * runs pq.printkMin().
+     * @param arr The given array.
+     * @param pq The priority queue that all the numbers that have been scanned
      * so far, scanned into this specific priotity queue.
-     * @return the number of numbers that have been scanned from the given
-     * arrey after the itteration of this method.
+     * @return The number of numbers that have been scanned from the given
+     * array after the itteration of this method.
      */
     private static int whileScannedIsSmallerThenCheckpoint
     (int scanned, int checkpoint, double[] arr, PriorityQueue pq)
@@ -102,13 +99,13 @@ class Maman16
         return scanned;        //line 6
     }
     /**
-     * the method gets the current checkpoint and returns the next
+     * The method gets the current checkpoint and returns the next
      * checkpoint in this order: n1 -> n2 -> n3 -> n
      * @param n1, n2, n3, n the given n1, n2, n3, n.
-     * range: n1<n2<n3<n, all of them natural numbers or 0.
-     * @param checkpoint the current checkpoint,
-     * range: one of the given n1, n2, n3. [not n].
-     * @return the next checkpoint in this order: n1 -> n2 -> n3 -> n
+     * Range: n1<n2<n3<n, all of them are natural numbers or 0.
+     * @param checkpoint The current checkpoint,
+     * Rnge: one of the given n1, n2, n3. [not n].
+     * @return The next checkpoint in this order: n1 -> n2 -> n3 -> n
      */
     public static int promoteCheckpoint
     (int checkpoint, int n1, int n2, int n3, int n)
@@ -119,16 +116,16 @@ class Maman16
         return -1; //the method will not get to here
     }
     /**
-     * the method scans the numbers in the given array until each one
-     * of the checkpoints (n1,n2,n3 and n). When it get to a checkpoint, the
-     * method prints the k smallest numbers that have been scanned
+     * The method scans the numbers in the given array until each one
+     * of the checkpoints (n1,n2,n3 and n). When it reaches a checkpoint, the
+     * method prints the k lowest numbers that have been scanned
      * so far. The checkpoint is being updated after that.
-     * @param arr,k,n1,n2,n3 the given arrk,,n1,n2,n3.
-     * range: arr is arrey of real numbers. k is a natural number.
-     * n1,n2,n3 are non-negatuve integer numbers that
-     * maintain n1<n2<n3<arr.length. 
-     * @param pq an the priority queue in the size of k, that into it,
-     * all the numbers from the array will be inserted. range: an empty
+     * @param arr,k,n1,n2,n3 The given arr, k, n1 ,n2 ,n3.
+     * Range: arr is array of real numbers. k is a natural number.
+     * n1,n2,n3 are non-negatuve integer numbers such that
+     * n1<n2<n3<arr.length. 
+     * @param pq The priority queue in the size of k which the method
+     * will insert the numbers from the array. range: an empty
      * priority queue in the size of k.
      * @param scanned number of numbers that have been scanned from
      * the arrey at a current point of time.
@@ -150,29 +147,27 @@ class Maman16
         }      //line 9
     }
     /**
-     * the method gets an array of real numbers, size k, 3 check points
+     * The method is given an array of real numbers, a size k and 3 check points
      * n1,n2,n3. If k=0, the method returns array of real numbers in
      * length 0. Otherwise, the method initialize a new priority queue
      * in size k: (pq).
      * The method inserts each element of the array into pq. After
      * insrting n1 elements, after inserting n2 elements and after 
      * inserting n3 elements, the method
-     * prints the k minimum numbers so far. After inserting all the 
+     * prints the k minimum numbers at the current point. After inserting all the 
      * elements of arr into pq, the method initialize a new list (array)
      * and inserts to the list the k
-     * or the n numbers that are located in pq. This insertion is being
-     * done like an in-order walk on pq.tree - therefore, the numbers
-     * are being
-     * inserted to the list in a sorted way (smallest to greatest).
+     * or the n numbers that are held by pq. This is done by in-order
+     * walking on pq.tree - therefore, the numbers are being 
+     * inserted to the list in a sorted way (lowest to greatest).
      * The method returns the required list.
-     * ranges: arr: an arrey of real numbers in a length of at
-     * least 3 [explaination why 3 in 3 lines ahead].
-     * k: a non-negative integer,
-     * n1,n2,n3: non-negative integers that maintain
-     * n1<n2<n3<arr.length. therefore arr.length must be equal or
-     * greater then 3.
-     * @param n the length of arr
-     * @param theRequiredList the list that is being returned (in
+     * Ranges: arr: An array of size 3 at least.
+     * k: A non-negative integer,
+     * n1,n2,n3: Non-negative integers that satisfies
+     * n1<n2<n3<arr.length. (This is the reson to the
+     * constraint arr.length >=3).
+     * @param n The length of arr
+     * @param theRequiredList The list that is being returned (in
      * case that k!=0).
      */
     public static double[] lowestK
